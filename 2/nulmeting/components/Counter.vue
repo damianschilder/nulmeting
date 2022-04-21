93501d07fc74c37d019ce93448e8095830282272
damianschilder <damiancschilder@gmail.com>
2022-04-21 18:08:41 +0200

    Files

---
+++ 2/nulmeting/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="Counter">
+	<button v-on:click="count++">Aantal keer geklikt:</button>
+	<h1>{{ count }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return{
+			count: 0
+		}
+	}
+}
+</script>
